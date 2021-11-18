9c9587746a306e541ca92e826526a45a259962cd
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2021-11-18 23:48:31 +0700

    update UI

--- screens/Rewards.js
+++ screens/Rewards.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { Divider, Text, Drawer, Appbar, ProgressBar, Colors   } from 'react-native-paper';
-import { useIsFocused } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFonts } from "expo-font";
+
+
 function Rewards({navigation}) {
-  
+  const [loaded] = useFonts({
+    Kanit: require("../assets/fonts/Kanit-Regular.ttf"),
+  });
   const [score, setScore] = React.useState(localStorage.getItem('score'))
 
   //hook when click tab navigation to re-render
@@ -50,30 +53,38 @@ function Rewards({navigation}) {
     <View style={{flex:1}}>
       {/* ส่วนที่ 1 */}
       <View style={{backgroundColor: "white", alignItems: 'center', justifyContent: 'center', margin: 20}} >
-        <Text style={{fontSize: 35}}>รางวัลความสำเร็จ</Text>
-        <Text>แต้มชนะสะสมของคุณคือ : {score}</Text>
+        <Text style={{fontSize: 35, fontFamily: 'Kanit'}}>รางวัลความสำเร็จ</Text>
+        <Text style={{fontSize: 16, fontFamily: 'Kanit'}}>แต้มชนะสะสมของคุณคือ : {score}</Text>
       </View>
 
       <ProgressBar progress={progress*0.08} color={Colors.yellow800} style={{height:30}}/>
       {/* ส่วนที่ 2 */}
       {/* https://materialdesignicons.com/ */}
       <View style={{ flex: 3, backgroundColor: "#9575CD", alignItems: 'center'}} >
-        <Text style={{fontSize: 35, color: "white"}}> ฉายา </Text>
+        <Text style={{fontSize: 35, color: "#fff", fontFamily: 'Kanit'}}> ฉายา </Text>
         <ScrollView>
-          <Text>เล่นเกม Quiz ชนะ 1 ครั้ง</Text>
-          {score >= 1 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="star" label="เด็กฝึกหัด"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 5 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="star" label="เป็ดเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 10 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="star-outline" label="คนจรเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 20 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="star-circle" label="กุลีเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 30 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="trophy-outline" label="นักเล่นคำฝึกหัด"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 50 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="trophy-outline" label="นักเล่นคำฉมัง"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 70 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="trophy-outline" label="ยอดนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 100 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="decagram-outline" label="วีรชนนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          {score >= 125 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="check-decagram" label="ตำนานนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 1 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 1 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="baby-face" label="เด็กฝึกหัด"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 5 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 5 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="duck" label="เป็ดเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 10 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 10 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="account" label="คนจรเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 20 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 20 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="hiking" label="กุลีเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 30 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 30 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="account-hard-hat" label="นักเล่นคำฝึกหัด"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 50 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 50 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="face-agent" label="นักเล่นคำมือฉมัง"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 70 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 70 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="incognito" label="ยอดนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 100 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 100 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="sword-cross" label="วีรชนนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 125 ครั้ง เพื่อปลดล็อค</Text>
+          {score >= 125 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="meditation" label="ตำนานนักเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 150 ครั้ง เพื่อปลดล็อค</Text>
           {score >= 150 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="crown" label="เทพแห่งการเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
+          <Text style={{color: '', fontSize: 18, fontFamily: 'Kanit'}}>เล่นเกม Quiz ชนะ 200 ครั้ง เพื่อปลดล็อค</Text>
           {score >= 200 ? (<Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="chess-king" label="เทวราชแห่งการเล่นคำ"/>) : <Drawer.Item style={{ backgroundColor: 'white', width: 300 }} icon="" label="?????"/>}
-          
-          
         </ScrollView>
         
       </View>
