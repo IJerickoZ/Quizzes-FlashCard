541fa64c687504ba7ef973f5dd1bb3a6ceac6117
supakorn <supakorn.ping.k@gmail.com>
2021-12-09 20:02:57 +0700

    Update DicDetail.js

--- DicDetail.js
+++ DicDetail.js
@@ -1,8 +1,8 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import axios from "axios";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 import {
   Searchbar,
   Appbar,
@@ -14,27 +14,31 @@ import {
   Card,
   Title,
   Paragraph,
-  List
+  List,
 } from "react-native-paper";
 
 export default function DicDetail({ route }) {
   const { word, meaning } = route.params;
   const [card, setcard] = useState([]);
+  const [wordcheck, setwordcheck] = useState([]);
   const navigation = useNavigation();
   let result;
-  useEffect(()=>{
+  let check;
+  let sup = ["aa", "bb"]
+  let num;
+  useEffect(() => {
     const fetchData = async () => {
       let data = {
-        token: await AsyncStorage.getItem('token')
-      }
+        token: await AsyncStorage.getItem("token"),
+      };
       axios.post("http://localhost:3000/getcardItemAll", data).then((res) => {
-        console.log(res.data)
+        console.log(res.data);
         result = res.data;
         setcard(result);
       });
-    }
+    };
     fetchData();
-  },[])
+  }, []);
   return (
     <View style={{ flex: 1 }}>
       <Card style={{ margin: 10 }}>
@@ -58,20 +62,57 @@ export default function DicDetail({ route }) {
               }}
             />*/}
             <List.Accordion title="Add This Word">
-              {card.map((item)=>
-                <List.Item title={item.cardname} onPress={()=>{
-                  let data = {
-                    word: word,
-                    meaning: meaning[0].definitions[0].definition,
-                    in: item.cardSetNum,
-                  }
-                  axios.put("http://localhost:3000/updatecard", data)
+              {card.map((item) => (
+                <List.Item
+                  title={item.cardname}
+                  onPress={() => {
+                    let data = {
+                      word: word,
+                      meaning: meaning[0].definitions[0].definition,
+                      in: item.cardSetNum,
+                    };
+                    axios
+                      .get(
+                        "http://localhost:3000/getcardItem?search=" + data.in
+                      )
+                      .then((res) => {
+                        //console.log(res.data.cardList);
+                        //setwordcheck(res.data.cardList);
+                        check = res.data.cardList
+                        console.log(check.length)
+                        num = check.length
+                        console.log(num);
+                        if(num != 0){
+                          for (let i = 0; i < num; i++) {
+                            if (word == check[i].word) {
+                              console.log("No");
+                            }else if (i == check.length-1 && check[i].word != word) {
+                              axios
+                                .put("http://localhost:3000/updatecard", data)
+                                .then((res) => {
+                                  alert("Add Complete");
+                                  navigation.navigate("Dictionary");
+                                });
+                            }else if(i == check.length-1 && check[i].word == word){
+                              alert("this word is has in this card")
+                            }
+                          }
+                        }else{
+                          axios.put("http://localhost:3000/updatecard", data)
+                      .then((res)=>{
+                        alert("Add Complete")
+                        navigation.navigate('Dictionary')
+                      })
+                        }
+                      });
+                    /*axios.put("http://localhost:3000/updatecard", data)
                   .then((res)=>{
                     alert("Add Complete")
                     navigation.navigate('Dictionary')
-                  })
-                }}/>
-              )}
+                  })*/
+                  }}
+                />
+              ))}
               {/*<List.Item title="First item" />
               <List.Item title="Second item" />*/}
             </List.Accordion>
